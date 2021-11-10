6ff7ecf46f71dd59164856b7069e3fd6e9c2dd44
Sayiarín Hainsátè <sayiarin@gmail.com>
2021-11-10 11:28:05 +0100

    add texture combiner

---
+++ Editor/EditorWindow/ImageCombinerWindow.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SayiTools
+{
+    public class ImageCombinerWindow : EditorWindow
+    {
+        private const string FILE_ENDING = "png";
+        private const string ImageInfoMessage = "Combines multiple images into one, using the average colour of a pixel of one image and adding it to the R, G, B, or A channel of the target texture accordingly\nIf no texture is supplied the value will be assumed as 0 (black). Alpha Values in the source images will be discarded.";
+        private string OutputPath = "Assets";
+        private string OutputName = "CombinedImage";
+
+        private Texture2D ChannelR;
+        private Texture2D ChannelG;
+        private Texture2D ChannelB;
+        private Texture2D ChannelA;
+
+        private bool TexturesValid = true;
+        List<Texture2D> Textures = new List<Texture2D>();
+
+        [MenuItem("Tools/Sayi/Image Combiner")]
+        public static void Init()
+        {
+            GetWindow<ImageCombinerWindow>("Image Combiner").Show();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            GUILayout.Space(EditorGUIUtility.singleLineHeight);
+            EditorGUIHelper.HeaderLevel2("Image Combiner");
+            EditorGUILayout.HelpBox(ImageInfoMessage, MessageType.Info);
+            GUILayout.Space(EditorGUIUtility.singleLineHeight);
+
+            ChannelR = (Texture2D)EditorGUILayout.ObjectField("Red Channel Image", ChannelR, typeof(Texture2D), false);
+            ChannelG = (Texture2D)EditorGUILayout.ObjectField("Green Channel Image", ChannelG, typeof(Texture2D), false);
+            ChannelB = (Texture2D)EditorGUILayout.ObjectField("Blue Channel Image", ChannelB, typeof(Texture2D), false);
+            ChannelA = (Texture2D)EditorGUILayout.ObjectField("Alpha Channel Image", ChannelA, typeof(Texture2D), false);
+
+            // maybe not optimal to use a List<> here, but it's fast enough for this limited dataset
+            // and will be very helpful later too
+            Textures.Clear();
+            if (ChannelR) Textures.Add(ChannelR);
+            if (ChannelG) Textures.Add(ChannelG);
+            if (ChannelB) Textures.Add(ChannelB);
+            if (ChannelA) Textures.Add(ChannelA);
+
+            TexturesValid = TextureHelper.TexturesShareDimensionsAndFormat(Textures.ToArray());
+            if (Textures.Count != 0 && TexturesValid == false)
+            {
+                EditorGUILayout.HelpBox("All images have to be the same size!", MessageType.Error);
+            }
+
+            List<string> nonReadableFiles = new List<string>();
+            foreach ( Texture2D texture in Textures)
+            {
+                if (!texture.isReadable)
+                {
+                    nonReadableFiles.Add(texture.name);
+                }
+            }
+
+            if (nonReadableFiles.Count != 0)
+            {
+                EditorGUILayout.HelpBox(String.Format("The following images are not readable, please change the import settings of the Textures to 'Read/Write Enabled':\n{0}", String.Join(", ", nonReadableFiles)), MessageType.Error);
+            }
+
+            GUILayout.Space(EditorGUIUtility.singleLineHeight);
+            OutputName = EditorGUILayout.TextField("Name:", OutputName);
+            if (GUILayout.Button("Change path for generated file"))
+            {
+                OutputPath = EditorUtility.OpenFolderPanel("Output path for generated files", "", "");
+                if (OutputPath.StartsWith(Application.dataPath))
+                {
+                    OutputPath = "Assets" + OutputPath.Substring(Application.dataPath.Length);
+                }
+            }
+            EditorGUI.BeginDisabledGroup(!TexturesValid || Textures.Count == 0 || nonReadableFiles.Count != 0);
+            if (GUILayout.Button("Save"))
+            {
+                CombineAndSaveImage();
+            }
+            EditorGUI.EndDisabledGroup();
+            EditorGUILayout.HelpBox(String.Format("Current output folder for generated files:\n{0}", GetOutputPath()), MessageType.Info);
+            EditorGUILayout.EndVertical();
+        }
+
+        private void CombineAndSaveImage()
+        {
+            Texture2D combinedTexture = new Texture2D(Textures[0].width, Textures[0].height);
+
+            for (int y = 0; y < Textures[0].width; y++)
+            {
+                for (int x = 0; x < Textures[0].height; x++)
+                {
+                    float redChannel = SampleAverageScalarFromTexture(ChannelR, x, y);
+                    float greenChannel = SampleAverageScalarFromTexture(ChannelG, x, y);
+                    float blueChannel = SampleAverageScalarFromTexture(ChannelB, x, y);
+                    float alphaChannel = SampleAverageScalarFromTexture(ChannelA, x, y);
+                    Color combinedColor = new Color(redChannel, greenChannel, blueChannel, alphaChannel);
+                    combinedTexture.SetPixel(x, y, combinedColor);
+                }
+            }
+
+            combinedTexture.Apply();
+            byte[] bytes = combinedTexture.EncodeToPNG();
+            File.WriteAllBytes(GetOutputPath(), bytes);
+            AssetDatabase.Refresh();
+        }
+
+        private float SampleAverageScalarFromTexture(Texture2D texture, int x, int y)
+        {
+            if (texture)
+            {
+                Color color = texture.GetPixel(x, y);
+                float combinedScalarValue = color.r + color.g + color.b;
+                if (combinedScalarValue == 0)
+                {
+                    return 0.0f;
+                }
+                return combinedScalarValue / 3;
+            }
+            return 0.0f;
+        }
+
+        private string GetOutputPath()
+        {
+            return $"{OutputPath}/{OutputName}.{FILE_ENDING}";
+        }
+    }
+}
---
+++ Editor/EditorWindow/NoiseGeneratorWindow.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+namespace SayiTools
+{
+    public class NoiseGeneratorWindow : EditorWindow
+    {
+        private enum TextureSize { x64 = 64, x128 = 128, x256 = 256, x512 = 512, x1024 = 1024, x2048 = 2048 };
+        private const string FILE_ENDING = "png";
+        private const RenderTextureFormat RENDER_TEXTURE_FORMAT = RenderTextureFormat.ARGB32;
+        private const int PREVIEV_SIZE = 300;
+
+        private TextureSize TexSize = TextureSize.x64;
+        private float Scale = 10f;
+
+        // base path as fallback, but that the path should be set properly by the
+        // context menu that spawns the window
+        private string OutputPath = "Assets";
+        private string OutputName = "Noise";
+
+        private Texture2D NoiseTexture;
+
+        [MenuItem("Tools/Sayi/Noise Generator")]
+        public static void Init()
+        {
+            GetWindow<NoiseGeneratorWindow>("Noise Generator").Show();
+        }
+
+        private void OnEnable()
+        {
+            NoiseTexture = new Texture2D(64, 64);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Space(EditorGUIUtility.singleLineHeight);
+            EditorGUIHelper.HeaderLevel2("Noise Generator");
+            OutputName = EditorGUILayout.TextField("Name:", OutputName);
+            TexSize = (TextureSize)EditorGUILayout.EnumPopup("Texture Size:", TexSize);
+            Scale = EditorGUILayout.Slider("Noise Level:", Scale, 1f, 500f);
+
+            if (NoiseTexture)
+            {
+                GUI.DrawTexture(new Rect(5, 250, PREVIEV_SIZE, PREVIEV_SIZE), NoiseTexture);
+            }
+
+            if (GUILayout.Button("Change path for generated file"))
+            {
+                OutputPath = EditorUtility.OpenFolderPanel("Output path for generated files", "", "");
+                if (OutputPath.StartsWith(Application.dataPath))
+                {
+                    OutputPath = "Assets" + OutputPath.Substring(Application.dataPath.Length);
+                }
+            }
+            if (GUILayout.Button("Generate"))
+            {
+                GeneratePerlinNoise();
+            }
+
+            if (GUILayout.Button("Save"))
+            {
+                SaveNoise();
+            }
+            EditorGUILayout.HelpBox(String.Format("Current output folder for generated files:\n{0}", GetOutputPath()), MessageType.Info);
+
+            GUILayout.Space(PREVIEV_SIZE + 10);
+            GUILayout.EndVertical();
+        }
+
+        private void GeneratePerlinNoise()
+        {
+            int size = (int)TexSize;
+            Color[] pixels = new Color[size*size];
+            NoiseTexture.Resize(size, size);
+
+            for (float y = 0.0f; y < size; y += 1.0f)
+            {
+                for (float x = 0.0f; x < size; x += 1.0f)
+                {
+                    float xCoord = x / size * Scale;
+                    float yCoord = y / size * Scale;
+                    float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                    pixels[(int)(y*size + x)]= new Color(sample,sample,sample);
+                }
+            }
+
+            NoiseTexture.SetPixels(pixels);
+            NoiseTexture.Apply();
+        }
+
+        private void SaveNoise()
+        {
+            if (!NoiseTexture)
+            {
+                Debug.LogError("You have to generate the Noise before saving it :>");
+                return;
+            }
+
+            byte[] textureData = NoiseTexture.EncodeToPNG();
+            File.WriteAllBytes(GetOutputPath(), textureData);
+
+            // refreshing after file has been written, it works on my machine lol
+            AssetDatabase.Refresh();
+        }
+
+        private RenderTexture CreateRenderTexture(int size)
+        {
+            RenderTexture renderTexture = new RenderTexture(size, size, 0, RENDER_TEXTURE_FORMAT);
+            renderTexture.enableRandomWrite = true;
+            renderTexture.wrapMode = TextureWrapMode.Repeat;
+            renderTexture.Create();
+            return renderTexture;
+        }
+
+        private void DrawComputeShaderNotFoundError()
+        {
+            GUIStyle style = new GUIStyle();
+            style.fontStyle = FontStyle.Bold;
+            style.normal.textColor = Color.red;
+            style.alignment = TextAnchor.MiddleCenter;
+            style.wordWrap = true;
+            EditorGUILayout.LabelField("Compute shader not found! Please make sure you download the full project from my gitlab and the PerlinNoiseGenerator.compute file exists at the correct location!", style);
+            if (GUILayout.Button("Close"))
+            {
+                this.Close();
+            }
+        }
+
+        public void SetOutputPath(string outputPath)
+        {
+            this.OutputPath = outputPath;
+        }
+
+        private string GetOutputPath()
+        {
+            return $"{OutputPath}/{OutputName}.{FILE_ENDING}";
+        }
+    }
+}
