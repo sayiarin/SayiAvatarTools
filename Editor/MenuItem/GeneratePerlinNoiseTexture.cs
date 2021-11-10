801916bad946ac1284e08e44d9ab9fdce7b394be
Sayiarín Hainsátè <sayiarin@gmail.com>
2021-11-10 11:26:25 +0100

    add simple perlin noise generator

---
+++ Editor/MenuItem/GeneratePerlinNoiseTexture.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SayiTools {
+
+    public class GeneratePerlinNoiseTexture : ScriptableObject
+    {
+        [MenuItem("Assets/Sayi Tools/Generate Perlin Noise Texture", false, 100)]
+        private static void OpenPerlinNoiseGeneratorWindow()
+        {
+            NoiseGeneratorWindow window = ScriptableObject.CreateInstance<NoiseGeneratorWindow>();
+            window.position = new Rect(Screen.width / 2, Screen.height / 2, 300, 500);
+            window.Show();
+
+            string outputPath = "Assets";
+            UnityEngine.Object activeObject = Selection.activeObject;
+            if (activeObject != null)
+            {
+                outputPath = AssetDatabase.GetAssetPath(activeObject.GetInstanceID());
+                if (AssetDatabase.IsValidFolder(outputPath) == false)
+                {
+                    outputPath = outputPath.Remove(outputPath.LastIndexOf('/'));
+                }
+            }
+            window.SetOutputPath(outputPath);
+        }
+    }
+}
